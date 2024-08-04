630a409e276d6ecf891181bceb74d1e3913c62fc
Hugo Uraga <hugouraga61@gmail.com>
2024-08-04 10:50:54 -0300

    feat: creates User repository

---
+++ backend/src/domain/repository/user.repository.ts
@@ -0,0 +1,8 @@
+import { User } from '@/domain/entity/user.entity';
+
+export abstract class UserRepository {
+  abstract getById(id: string): Promise<User | null>;
+  abstract getByEmail(email: string): Promise<User | null>;
+  abstract create(user: User): Promise<User>;
+  abstract update(id: string, updatedUser: Partial<User>): Promise<User>;
+}
