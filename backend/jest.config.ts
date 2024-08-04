19e37d50dfc8fb946067bf4af68c31c9a61f206a
Hugo Uraga <hugouraga61@gmail.com>
2024-08-04 12:47:25 -0300

    feat: jest config paths

---
+++ backend/jest.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from '@jest/types';
+import { pathsToModuleNameMapper } from 'ts-jest';
+import { compilerOptions } from './tsconfig.json';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/',
+  }),
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  rootDir: './',
+  testRegex: '.*\\.spec\\.ts$',
+};
+
+export default config;
