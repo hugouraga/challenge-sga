74a67d2d83a8d5cf839dec97a23b2e07da53f486
Hugo Uraga <hugouraga61@gmail.com>
2024-08-04 11:31:10 -0300

    feat: implement in-memory user repository

---
+++ backend/src/infra/database/memory/in-memory-user-repository.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@nestjs/common';
+import { UserRepository } from '@/domain/repository/user.repository';
+import { User } from '@/domain/entity/user.entity';
+
+@Injectable()
+export class InMemoryUserRepository implements UserRepository {
+  private userList: User[] = [];
+
+  async getById(id: string): Promise<User> {
+    const user = this.userList.find((user) => user.id === id);
+    if (!user) throw new Error('User not found');
+    return user;
+  }
+
+  async getByEmail(email: string): Promise<User> {
+    const user = this.userList.find((user) => user.email === email);
+    if (!user) throw new Error('User not found');
+    return user;
+  }
+
+  async create(user: User): Promise<User> {
+    this.userList.push(user);
+    return user;
+  }
+
+  async update(id: string, updatedUser: Partial<User>): Promise<User> {
+    const index = this.userList.findIndex((user) => user.id === id);
+    if (index === -1) throw new Error('User not found');
+
+    const currentUser = this.userList[index];
+    currentUser.update(updatedUser);
+
+    this.userList[index] = currentUser;
+    return currentUser;
+  }
+
+  async signIn(email: string, password: string): Promise<User> {
+    const user = this.userList.find((user) => user.email === email);
+    if (!user) throw new Error('Invalid email or password');
+
+    const isPasswordValid = await user.comparePassword(password);
+    if (!isPasswordValid) throw new Error('Invalid email or password');
+
+    return user;
+  }
+}
