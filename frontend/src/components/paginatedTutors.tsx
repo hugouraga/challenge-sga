5c73302bf1036df94b7906b6907aa01aff2dac17
Hugo Uraga <hugouraga61@gmail.com>
2024-08-09 04:42:08 -0300

    feat: create pagination component

---
+++ frontend/src/components/paginatedTutors.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import React, { useState } from 'react';
+import { Box, Stack, Pagination, Typography } from '@mui/material';
+import TutorialItem from './TutorItem';
+import { tutorProps } from '@/interfaces/tutor.interface';
+
+interface Props {
+  filteredTutors: tutorProps[];
+  selectedTutor: tutorProps | null;
+  handleTutorClick: (tutor: tutorProps) => void;
+}
+
+const TutorsList: React.FC<Props> = ({ filteredTutors, handleTutorClick, selectedTutor }) => {
+  const [page, setPage] = useState<number>(1);
+  const itemsPerPage = 6;
+
+  const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
+    setPage(value);
+  };
+
+  const paginatedTutors = filteredTutors?.slice((page - 1) * itemsPerPage, page * itemsPerPage);
+
+  return (
+    <>
+      {paginatedTutors?.length > 0 ? (
+        paginatedTutors.map((tutor, index) => (
+          <TutorialItem
+            key={tutor.id}
+            tutor={tutor}
+            index={index}
+            selected={selectedTutor?.id === tutor.id}
+            onClick={() => handleTutorClick(tutor)}
+          />
+        ))
+      ) : (
+        <Box sx={{ textAlign: 'center', mt: 'auto', mb: 'auto', display: 'flex', flexDirection: 'column', justifyContent: 'center', height: '100%', marginTop: -12 }}>
+          <Typography variant="h6">Nenhum usuário identificado</Typography>
+        </Box>
+      )}
+
+      <Box sx={{ textAlign: 'right', display: 'flex', flexDirection: 'column', alignItems: 'flex-end', marginRight: 1, marginTop: 3 }}>
+        <Stack spacing={2}>
+          <Pagination
+            count={Math.ceil(filteredTutors?.length / itemsPerPage)}
+            page={page}
+            onChange={handleChange}
+            variant="outlined"
+            shape="rounded"
+          />
+        </Stack>
+      </Box>
+    </>
+  );
+};
+
+export default TutorsList;
