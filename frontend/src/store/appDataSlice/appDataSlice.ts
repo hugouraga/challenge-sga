ccb6899a5f2ed89dd8eceb44e578cd2ea79c4fe7
Hugo Uraga <hugouraga61@gmail.com>
2024-08-09 06:10:52 -0300

    fix: ensure API URL fallback to localhost in case environment variable is undefined

--- frontend/src/store/appDataSlice/appDataSlice.ts
+++ frontend/src/store/appDataSlice/appDataSlice.ts
@@ -118,7 +118,7 @@ export const deleteTutorial = createAsyncThunk(
   async (tutorialId: string) => {
     const token = localStorage.getItem('token');
     await fetch(
-      `${process.env.NEXT_PUBLIC_API_URL ?? 'http://localhost:3333'/tutorial/delete/${tutorialId}`,
+      `${process.env.NEXT_PUBLIC_API_URL ?? 'http://localhost:3333'}${tutorialId}`,
       {
         method: 'DELETE',
         headers: {
